06c2a0673eac67727eeff2ec5923736bd3a0388f
tmexia <teresa.mexia@gmail.com>
2025-01-24 17:30:53 +0000

    added the API and completed search engine

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+function displayWeather(response) {
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature-value");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = currentTemperature;
+}
+
+function searchCity(city) {
+  let apiKey = "2affcb912c0bbco36ateff4a191143bb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
