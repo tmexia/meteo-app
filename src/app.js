e44d9593afc7f76121c1a4acbf337c4ba7947f72
tmexia <teresa.mexia@gmail.com>
2025-01-24 18:00:07 +0000

    added the date and time

--- src/app.js
+++ src/app.js
@@ -22,3 +22,47 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
+
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let weekday = days[now.getDay()];
+let date = now.getDate();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+console.log(weekday, date, month, year, hour, minutes);
+
+let formattedDate = `${weekday}, ${date} ${month} ${year} ${hour}:${minutes}`;
+console.log(formattedDate);
+
+function formatDate() {
+  let formattedDate = `${weekday}, ${date} ${month} ${year} ${hour}:${minutes}`;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formattedDate;
+}
+
+formatDate(new Date());
