227de5b5897fbfcc482bd44fad6012325c6a0125
tmexia <teresa.mexia@gmail.com>
2025-01-24 17:36:53 +0000

    Added Lisbon as searched city
    Added Lisbon as searched city for site default when opening

--- src/app.js
+++ src/app.js
@@ -20,3 +20,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
